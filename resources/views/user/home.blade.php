6992bb3571a8832558f48ad6c2eebc6bde49703b
ZB15 <zrinko.budimir@gmail.com>
2017-01-20 19:34:14 +0100

    add

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -9,6 +9,28 @@
   </ol>
 </div>
 <div class="row">
-
+	<div class="col-md-3">
+		<div class="list-group">
+			<a href="#" class="list-group-item active">Folders &amp; Files </a>
+			<a href="{{route('categories')}}" class="list-group-item">Categories</a>
+			<a href="#" class="list-group-item">Shared</a>
+		</div>	
+	</div>
+	<div class="col-md-9">
+		<div class="panel panel-default">
+			<div class="panel-heading clearfix">
+				<h3 class="pull-left panel-title">Folders &amp; Files</h3>
+				<div class="pull-right">
+					<a href="">
+						<span class="pull-right glyphicon glyphicon-folder-close" aria-hidden="true"></span>
+						<span class="pull-right glyphicon glyphicon-plus" aria-hidden="true"></span>
+					</a>
+				</div>
+			</div>
+			<div class="panel-body">
+				
+			</div>
+		</div>
+	</div>
 </div>
 @stop
