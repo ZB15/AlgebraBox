8005fdaf63f58a845116db1fa87b0a48197b9b2b
ZB15 <zrinko.budimir@gmail.com>
2017-01-23 22:11:08 +0100

    SAD RADI

---
+++ resources/views/user/categories/new.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.index')
+
+@section('title', 'New category')
+
+@section('content')
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">New</h3>
+            </div>
+            <div class="panel-body">
+                <form accept-charset="UTF-8" role="form" method="post" action="{{ route('user.categories.create') }}">
+					<fieldset>
+						<input class="form-control" placeholder="Category name" name="name" type="text">
+						<input name="_token" value="{{ csrf_token() }}" type="hidden">
+						<input class="btn btn-lg btn-primary btn-block" type="submit" value="Save">
+					</fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
